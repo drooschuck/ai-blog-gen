ca1402b4e9f8397e785e855c8402ea951a70ab8b
drooschuck <114895991+drooschuck@users.noreply.github.com>
2024-12-18 22:23:19 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,39 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Exit immediately if a command exits with a non-zero status
-#set -e
+set -o errexit
 
-# Print each command before executing it
-#set -x
-
-# Step 1: Set up Python environment
-# You can specify the Python version you want to use
-#PYTHON_VERSION="3.9"  # Change this to your desired Python version
-#pyenv install -s $PYTHON_VERSION
-#pyenv global $PYTHON_VERSION
-
-# Step 2: Upgrade pip
-pip install --upgrade pip
-
-# Step 3: Install dependencies
-# Make sure you have a requirements.txt file in your project root
+# Install dependencies
 pip install -r requirements.txt
 
-# Step 5: Collect static files
-# This is important for serving static files in production
-python manage.py collectstatic --noinput
+# Collect static files
+python manage.py collectstatic --no-input
 
-# Step 4: Run migrations
-# Make sure to set the environment variable for your database
-python manage.py migrate --noinput
+# Run database migrations
+python manage.py migrate
 
+# Create a superuser if it doesn't exist
+python - <<EOF
+import os
+from django.core.management import call_command
+from django.contrib.auth import get_user_model
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ai_blog_app.settings")
+import django
+django.setup()
 
-# Step 6: (Optional) Run tests
-# Uncomment the following line if you want to run tests before deployment
-# python manage.py test
-
-# Step 7: Start the application
-# This command may vary depending on how you want to run your Django app
-# For example, using Gunicorn:
-#gunicorn myproject.wsgi:application --bind 0.0.0.0:8000
+User  = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'drooschuck@gmail.com', 'admin@1234')
+EOF
