5a9ed677c643466bcad179d29390309d0a521081
drooschuck <114895991+drooschuck@users.noreply.github.com>
2024-12-18 23:11:11 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,31 +3,8 @@
 # Exit immediately if a command exits with a non-zero status
 set -o errexit
 
-# Function to fix APT lists directory
-fix_apt_lists() {
-    local dir="/var/lib/apt/lists/partial"
-
-    # Check if the directory exists
-    if [ ! -d "$dir" ]; then
-        echo "Directory $dir is missing. Creating it..."
-         mkdir -p "$dir"
-         chmod 755 "$dir"
-        echo "Directory $dir created and permissions set."
-    else
-        echo "Directory $dir already exists."
-    fi
-}
-
-# Fix APT lists directory
-fix_apt_lists
-
-# Update package list
-echo "Updating package lists..."
-sudo apt-get update
-
 # Update package list and install necessary packages
-apt-get update
-apt-get install -y python3 python3-pip python3-venv
+apt-get update && apt-get install -y python3 python3-pip python3-venv
 
 # Create a virtual environment
 python3 -m venv venv
@@ -62,5 +39,4 @@ if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'drooschuck@gmail.com', 'admin@1234')
 EOF
 
-# Deactivate the virtual environment
-deactivate
+echo "Build process completed successfully."
